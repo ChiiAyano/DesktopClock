f82b21e0b78e206f5681d1d06da65f1f1b319164
綾野ちい (Chii Ayano) <git@aynv.jp>
2025-08-07 07:54:58 +0900

    プロジェクト ファイルを追加します。

---
+++ DesktopClock/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Interop;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+namespace DesktopClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int WindowOffset = 20;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += OnMainWindowLoad;    
+        }
+
+        private void OnMainWindowLoad(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new HWND(new WindowInteropHelper(this).Handle);
+            var extendedStyle = (WINDOW_EX_STYLE)PInvoke.GetWindowLong(hwnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE);
+
+            // ウィンドウに対するクリック操作を透過
+            _ = PInvoke.SetWindowLong(hwnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE, (int)(extendedStyle | WINDOW_EX_STYLE.WS_EX_TRANSPARENT));
+
+            // ウィンドウを右下に配置
+            var screen = Screen.PrimaryScreen;
+            if (screen is null)
+            {
+                return;
+            }
+
+            var workingArea = screen.WorkingArea;
+            var(width, height) = (this.Width, this.Height);
+
+            this.Left = workingArea.Right - width - WindowOffset;
+            this.Top = workingArea.Bottom - height - WindowOffset;
+        }
+    }
+}
---
+++ DesktopClock/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.TinyLinq;
+
+namespace DesktopClock.ViewModels;
+
+internal class MainPageViewModel
+{
+    private readonly ReactiveTimer _clock = new(TimeSpan.FromSeconds(0.1));
+
+    private ReactiveProperty<DateTimeOffset> Current { get; }
+    public ReactiveProperty<double> HourAngle { get; }
+    public ReactiveProperty<double> MinuteAngle { get; }
+    public ReactiveProperty<double> SecondAngle { get; }
+
+    public MainPageViewModel()
+    {
+        this.Current = _clock
+            .Select(_ => DateTimeOffset.Now)
+            .ToReactiveProperty(DateTimeOffset.Now);
+
+        this.HourAngle = this.Current
+            .Select(s => ((s.Hour % 12) * 30) + (s.Minute * 0.5))
+            .ToReactiveProperty();
+
+        this.MinuteAngle = this.Current
+            .Select(s => s.Minute * 6 + s.Second * 0.1)
+            .ToReactiveProperty();
+
+        this.SecondAngle = this.Current
+            .Select(s => 360d / 60d * (s.Second + (s.Millisecond / 1000d)))
+            .ToReactiveProperty();
+
+        _clock.Start();
+    }
+}
